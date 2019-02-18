15d78b69fc66ed3a8c46b7aa13052eda77cf1ae5
alexgQQ <alex.grand@beatport.com>
2019-02-18 11:56:39 -0700

    Add functionality for pymunk simulation integration

--- app/base.py
+++ app/base.py
@@ -95,7 +95,7 @@ class BaseShape:
     """
     Base class to control shape generation.
     """
-    def __init__(self, canvas, origin=None, start_time=0.0, color_scheme=BaseColorScheme):
+    def __init__(self, canvas=None, duration=None, origin=None, start_time=0.0, color_scheme=BaseColorScheme):
         self.canvas = canvas
         # Origin location of shape
         self.origin = origin
@@ -103,7 +103,7 @@ class BaseShape:
             self.origin = (random.randint(0, self.canvas.width - 1),
                            random.randint(0, self.canvas.height - 1))
         # Duration of shape in seconds
-        self.duration = random.uniform(3.0, 4.0)
+        self.duration = duration or random.uniform(3.0, 4.0)
         # Current time of creation
         self.time_start = start_time
         self.color_class = color_scheme
---
+++ app/physics.py
@@ -0,0 +1,109 @@
+import time
+import random
+import numpy as np
+import argparse
+import pymunk
+
+from base import BaseCanvas, BaseColorScheme, FrameRunner, BaseShape, ColorHSV
+from neopixel import Adafruit_NeoPixel, Color
+from config import LED_MATRIX_CONFIG
+
+
+class BouncyBalls(object):
+    def __init__(self):
+        # Space
+        self._space = pymunk.Space()
+        self._space.gravity = (0.0, -900.0)
+
+        # Time step
+        self._dt = 1.0 / 60.0
+        # Number of physics steps per screen frame
+        self._physics_steps_per_frame = 1
+
+        # Static barrier walls (lines) that the balls bounce off of
+        self._add_static_scenery()
+
+        # Points that exist in the world
+        self._balls = []
+    
+    def _step(self):
+        self._space.step(self._dt)
+
+    def _encode(self):
+        points = []
+        for ball in self._balls:
+            x, y = ball._body.position.int_tuple
+            x /= 10
+            y /= 10
+            points.append((int(x), int(y)))
+        return points
+
+    def _add_static_scenery(self):
+        width = 320.0
+        height = 80.0
+        static_body = self._space.static_body
+        static_lines = [pymunk.Segment(static_body, (0.0, 0.0), (width, 0.0), 0.0),
+                        pymunk.Segment(static_body, (0.0, 0.0), (0.0, height), 0.0),
+                        pymunk.Segment(static_body, (width, height), (0.0, height), 0.0),
+                        pymunk.Segment(static_body, (width, height), (width, 0.0), 0.0),]
+        for line in static_lines:
+            line.elasticity = 0.95
+            line.friction = 0.9
+        self._space.add(static_lines)
+
+    def _create_ball(self):
+        mass = 10
+        radius = 5
+        inertia = pymunk.moment_for_circle(mass, 0, radius, (0, 0))
+        body = pymunk.Body(mass, inertia)
+        body.position = 160, 70
+        shape = pymunk.Circle(body, radius, (0, 0))
+        shape.elasticity = 0.95
+        shape.friction = 0.9
+        self._space.add(body, shape)
+        self._balls.append(shape)
+
+
+class Canvas(BaseCanvas):
+    def __init__(self, space):
+        super().__init__()
+        self.space = space
+
+    def update(self):
+        super().update()
+        self.space.step()
+
+
+class Shape(BaseShape):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.body = kwargs.get('body', None)
+
+    def on_exit(self):
+        pass
+
+    def generator(self, _time):
+        return [self.body._body.position.int_tuple]
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Config options for flare light application')
+
+    parser.add_argument('-n', action="store", dest="n", type=int, const=1, nargs='?', default=1)
+    results = parser.parse_args()
+
+    num_of_lights = results.n
+
+    print('Starting Adafruit LED driver...')
+    led = Adafruit_NeoPixel(**LED_MATRIX_CONFIG)
+    led.begin()
+
+    sim = BouncyBalls()
+    sim._create_ball()
+    point = sim._balls[0]
+
+    print('Running animation...')
+    canvas = Canvas()
+    canvas.shapes = [Shape(canvas=canvas, origin=point._body.position.int_tuple, body=point) for _ in range(num_of_lights)]
+    runner = FrameRunner(led, canvas=canvas, debug=False)
+    runner.run()
