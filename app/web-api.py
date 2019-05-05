62f785bda387f9922b57f7eebb18ae3e03076c2a
alexgQQ <alex.grand@beatport.com>
2019-05-05 17:24:59 -0600

    Improve api

--- app/web-api.py
+++ app/web-api.py
@@ -1,16 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask.json import jsonify
 import numpy as np
 from neopixel import Adafruit_NeoPixel
 from config import LED_MATRIX_CONFIG
-from base import ColorRGB
+from base import ColorRGB, ColorHSV
 
 
 class LED:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self._led = Adafruit_NeoPixel(**LED_MATRIX_CONFIG)
         self._led.begin()
 
+        self.color_type = kwargs.get('color_type', 'RGB')
+        self.color_class = ColorHSV if self.color_type == 'HSV' else ColorRGB
         self.width = 32
         self.height = 8
         value = np.empty((), dtype=object)
@@ -18,12 +20,21 @@ class LED:
         self.board = np.full((self.width, self.height), value, dtype=object)
 
     def convert_board(self):
-        func = np.vectorize(ColorRGB.color)
-        self._led._led_data[:] = func(self.board).astype('int')
+        """ Converts color board to integers for led driver """
+        def color(values):
+            return self.color_class.color(*values)
 
-    def to_dict(self):
+        self._led._led_data[:] = np.apply_along_axis(color, 2, self.board).astype('int')
+        self._led.show()
+
+    def data(self):
         return dict(
             array=self.board.tolist()
+        )
+
+    def meta(self):
+        return dict(
+            color_type=self.color_type,
         )
 
 led = LED()
@@ -31,12 +42,13 @@ app = Flask(__name__)
 
 @app.route('/board', methods=['GET', 'POST'])
 def board():
-    if request.method == 'POST':
-        led.board = np.array(request.json.get('array'))
-        led.convert_board()
+    try:
+        if request.method == 'POST':
+            led.board = np.array(request.json.get('array'))
+            led.convert_board()
         return jsonify(led.to_dict())
-    else:
-        return jsonify(led.to_dict())
+    except Exception:
+        abort(400)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
